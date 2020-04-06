0d77710d36725325cc22a067970d4e3d8e8226bd
yangxufeng <xufeng.yang@unity3d.com>
2020-04-06 10:41:10 +0800

    feat(module): 1. 增加输入判断

--- src/script/main.js
+++ src/script/main.js
@@ -5,27 +5,66 @@ function main() {
 }
 
 function load() {
+  var context = document.getElementById('context');
+  var cache = [];
   document.onkeydown = function(e) {
+    console.log(e);
+    var current = document.getElementById('current');
+    if (!current) {
+      current = document.createElement('div');
+      current.setAttribute('id', 'current');
+      context.appendChild(current);
+    }
     var keyNum=window.event ? e.keyCode :e.which;
-    var context = document.getElementById('context');
-    var cache = context.innerHTML;
+    var line = current.innerHTML;
     switch (parseInt(keyNum)) {
-      case 8: 
-        cache = cache.substr(0,cache.length - 1);
-        console.log(cache);
+      case 8:
+        if (cache.length > 0) {
+          cache.length--;
+        }
+        line = cache.join('');
         break;
       case 13:
-        cache += '</br>';
+        var result = eval(line);
+        cache.push('</br>');
+        line += '</br>';
+        line += result;
+        break;
+      case 32:
+        cache.push('&nbsp;');
+        line += '&nbsp;';
+      case 37:
+        break;
+      case 38:
+        break;
+      case 39:
+        break;
+      case 40:
+        break;
+      case 9:
+        cache.push('&nbsp;&nbsp;');
+        line += '&nbsp;';
+        line += '&nbsp;';
         break;
       case 16:
       case 17:
       case 18:
+      case 19:
+      case 20:
+      case 91:
+      case 92:
         break;
       default:
-        console.log(e.key);
-        cache += e.key;
+        cache.push(e.key);
+        line += e.key;
     }
-    context.innerHTML = cache;
+    current.innerHTML = line;
+    return false;
+  }
+
+  document.onkeyup = function(e) {
+    console.log(e);
+
   }
 }
 
